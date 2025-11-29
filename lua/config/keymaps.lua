ece90c8ff1efb3fddd252e282b5507a8c4022ce8
Devin Slate <sl8t3@hotmail.com>
2025-11-29 12:44:06 -0700

    use old n/v line movement mappings

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+local map = vim.keymap.set
+
+-- ===========================================================
+-- General Mappings
+-- ===========================================================
+-- Move current line up/down
+map("n", "<leader>j", ":m+<CR>==", { desc = "Move Line Down" })
+map("n", "<leader>k", ":m-2<CR>==", { desc = "Move Line Up" })
+map("v", "<leader>j", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
+map("v", "<leader>k", ":m '<-2<CR>gv=gv", { desc = "Move Selection Up" })
