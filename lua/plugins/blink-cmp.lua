086e67f6956f277ce243fcd96da1292194c5101c
Devin Slate <sl8t3@hotmail.com>
2026-04-01 09:24:00 -0600

    disable blink on md files

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -2,7 +2,7 @@ return {
   "saghen/blink.cmp",
   opts = {
     enabled = function()
-      return not vim.b.blink_disable
+      return not vim.b.blink_disable and vim.bo.filetype ~= "markdown"
     end,
   },
 }
