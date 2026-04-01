00aec462b907a22fec8f4e0be0daf1f8253f81c5
Devin Slate <sl8t3@hotmail.com>
2026-04-01 09:29:51 -0600

    improve blink disable logic

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,8 +1,16 @@
+local no_complete_ft = {
+  markdown = true,
+  gitcommit = true,
+  NeogitCommitMessage = true,
+  text = true,
+  help = true,
+}
+
 return {
   "saghen/blink.cmp",
   opts = {
     enabled = function()
-      return not vim.b.blink_disable and vim.bo.filetype ~= "markdown"
+      return not vim.b.blink_disable and not no_complete_ft[vim.bo.filetype]
     end,
   },
 }
